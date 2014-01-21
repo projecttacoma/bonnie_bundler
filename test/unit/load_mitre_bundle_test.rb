b5e5342fe0102246ca3d786834c51d4fbd87314d
Fernandes <smoothclip@gmail.com>
2014-01-21 14:33:19 -0500

    Added simple tests for Measures:BundleLoader:load()

--- test/unit/load_mitre_bundle_test.rb
+++ test/unit/load_mitre_bundle_test.rb
@@ -107,4 +107,26 @@ class LoadMiteBundleTest < ActiveSupport::TestCase
     p5_ev['DENEX'].must_equal 0
   end
 
+  test "Loading a MITRE bundle from JSON via EP" do
+    dump_db
+    Measures::BundleLoader.load(@tiny_bundle, nil, nil, nil, 'ep')
+    Measure.all.count.must_equal 2
+    m0002 = Measure.where(hqmf_id: '40280381-3D61-56A7-013E-5D1EF9B76A48').first
+    m0004 = Measure.where(hqmf_id: '40280381-3D61-56A7-013E-5B2AA3493C42').first
+    assert !m0002.nil?
+    assert !m0004.nil?
+    m0002.title.must_equal "Appropriate Testing for Children with Pharyngitis"
+    m0002.populations.size.must_equal 1
+    m0002.population_criteria.keys.count.must_equal 4
+    m0004.title.must_equal "Initiation and Engagement of Alcohol and Other Drug Dependence Treatment"
+    m0004.populations.size.must_equal 6
+    m0004.population_criteria.keys.count.must_equal 7
+  end
+
+  test "Loading a MITRE bundle from JSON via EH" do
+    dump_db
+    Measures::BundleLoader.load(@tiny_bundle, nil, nil, nil, 'eh')
+    Measure.all.count.must_equal 0
+  end
+
 end
