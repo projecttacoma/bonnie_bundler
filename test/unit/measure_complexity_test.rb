1be39f41e42969fe66cedf3ae5c03f875b95fef9
krautscheid Krautscheid <krautscheid@mitre.org>
2014-07-11 12:18:39 -0400

    Measure complexity analysys code, based on Kamira code

---
+++ test/unit/measure_complexity_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class MeasureComplexityTest < ActiveSupport::TestCase
+
+  setup do
+    dump_db
+    bundle = File.new File.join('test','fixtures','bundle-tiny.zip')
+    Measures::BundleLoader.load(bundle, nil)
+  end
+
+  # Just a regression test
+  test "calculate complexity" do
+    Measure.count.must_equal 2
+    Measure.first.complexity.must_equal({ "DENEX"=>2, "NUMER"=>2, "IPP"=>8 })
+    Measure.last.complexity.must_equal({ "DENEX"=>1, "NUMER"=>2, "NUMER_1"=>6, "IPP"=>10, "IPP_1"=>11, "IPP_2"=>10 })
+  end
+
+end
